2eaa03508f0f0dd8232f71d795b979b5821a451f
Erin M <erin.michon@gmail.com>
2021-11-09 16:30:54 -0500

    added an event listener to continer div to initial question reactions

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,7 +1,4 @@
 /* ACCEPTANCE CRITERIA
-GIVEN I am taking a code quiz
-WHEN I click the start button
-THEN a timer starts and I am presented with a question
 WHEN I answer a question
 THEN I am presented with another question
 WHEN I answer a question incorrectly
@@ -11,52 +8,33 @@ THEN the game is over
 WHEN the game is over
 THEN I can save my initials and score */
 
-
-//define the variables and html elements that we will turn into objects and manipulate
+// **  PAGE VARIABLES **
+// Define the variables and html elements that we will turn into objects and manipulate
 let headerEl = document.querySelector("#header");
 let timerEl = document.querySelector("#timer");
 let containerEl = document.querySelector("#container");
 let startBtn = document.querySelector("#start-button");
+let answerEl = document.querySelector(".answer");
 
-// define a timer; this will be used to keep score; a wrong answer deducts time
-
-//Start the timer at 100 seconds (or 100 points for high score)
-let time = 100
-
-//create a function that starts displays the remaining time
-let countdown = function() {
-    time--;
-    if (time < 0) {
-        clearInterval(countdown);
-        return;
-    }
-    timerEl.innerHTML = time;
-};
-
-//create a function that starts the countdown (in seconds)
-let counter = function () {
-    setInterval(countdown, 1000);
-};
-
-// define the different questions and answers/options for the quiz
+// Define the different questions and answers/options for the quiz - Currently all correct answers are answerA 
 let myQuizQuestions = [
     {
-        question: "What can be passed into a function?",
+        question: "What can be passed into a function?", 
         answerA: "Arguements",
-        answerB: "Parameters",
+        answerB: "Comments",
         answerC: "Bananas",
     },
     {
-        question: "Do you know the answer?",
-        answerA: "right",
-        answerB: "wrong",
-        answerC: "wrong",
+        question: "Which option below is an example of camel-casing?",
+        answerA: "camel-walk",
+        answerB: "CamelWalk",
+        answerC: "camelWalk",
     },
     {
-        question: "Which of the following is a control flow statement?",
-        answerA: "A for Loop",
-        answerB: "Using an if statement",
-        answerC: "Defining the index of an array",
+        question: "Which of the following is NOT a control flow statement?",
+        answerA: "An if statement",
+        answerB: "A for loop",
+        answerC: "A while loop",
     },
     {
         question: "What does '===' mean when placed between two variables?",
@@ -72,12 +50,48 @@ let myQuizQuestions = [
     },
 ];
 
+//variables to obtain the index of a given question
 let lastQuestionIndex = myQuizQuestions.length - 1;
 let currentQuestionIndex = 0;
 
-function showQuestions (event) {
+// ** TIMER **
+
+// Start the timer at 100 seconds (or 100 points for high score)
+let time = 100
+
+// Create a function that starts displays the remaining time
+let countdown = function() {
+    time--;
+    if (time < 0) {
+        clearInterval(countdown);
+        return;
+    };
+
+    timerEl.innerHTML = time;
+    
+    // When the timer reaches zero, the timer reads game over
+    if (time == 0) {
+    timerEl.innerHTML = "You have run out of time!  GAME OVER!";
+
+    //call function that allow for user to input their initials and view high-score
+
+    };
+};
+
+// Create a function that starts the countdown (in seconds)
+let counter = function () {
+    setInterval(countdown, 1000);
+};
+
+
+// ** QUESTION CREATION AND DISPLAY
+
+// Create and display initial question
+let showQuestions = function(event) {
+    //prevent the browser from preforming any default actions
     event.preventDefault();
 
+    //hide the start button after it is pushed
     document.getElementById("start-button").style.display = "none"
 
     //change the DOM object to show the text/options from the first question
@@ -89,7 +103,7 @@ function showQuestions (event) {
     questionEl.innerHTML = "<h2 class='question'>" + currentQ.question + "</h2>";
     questionEl.className ="question";
     document.getElementById("container").appendChild(questionEl); 
-
+    
     let answersBtnA = document.createElement("button")
     answersBtnA.innerHTML = "<h3 class='answer'>" + currentQ.answerA + "</h3>";
     answersBtnA.className ="answer";
@@ -98,7 +112,7 @@ function showQuestions (event) {
     let answersBtnB = document.createElement("button")
     answersBtnB.innerHTML = "<h3 class='answer'>" + currentQ.answerB + "</h3>";
     answersBtnB.className ="answer";
-    containerEl.appendChild(answersBtnB);
+    containerEl.appendChild(answersBtnB); 
 
     let answersBtnC = document.createElement("button")
     answersBtnC.innerHTML = "<h3 class='answer'>" + currentQ.answerC + "</h3>";
@@ -107,17 +121,28 @@ function showQuestions (event) {
 
     //increase the current index by one for the next question
     currentQuestionIndex++;
-
-    //listen for a click on any button
-        //if false = subtract time
-        //if true = loop in next question for as long as there are questions remaining
 };
 
+// Reactions to the question being answered
+let nextQuestion = function(event) {
+    event.preventDefault();
+    
+    let userAnswer = $(event.target);
+    console.log(userAnswer);
+
+    // Check to see if the answer was correct
+    // If the answer was incorrect, subtrack 5 from current time (If)
+    // If the answer was correcct, do nothing  (Else If)
+    // Present the next set of questions
+    // Repeat until last question (While Loop)
+    
+}
 
 
-// listen for the click of the start button
+// ** EVENT LISTENERS **
 startBtn.addEventListener("click", showQuestions);
 startBtn.addEventListener("click", counter);
+containerEl.addEventListener("click", nextQuestion);
 
 
 //Additional Pseudocode to review/conquer:
@@ -126,18 +151,3 @@ startBtn.addEventListener("click", counter);
     //call a function to load the high scores from localStorage at end of script file
     
 
-
-// //function for when answers are clicked?
-// function userAnswerCorrect (event) {
-//     event.preventDefault();
-
-//     console.log("correct answer selected!")
-//     console.log(time)
-// };
-
-// function userAnswerWrong (event) {
-//     event.preventDefault();
-
-//     console.log("wrong answer selected!")
-//     console.log(time)
-// };
